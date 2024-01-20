fe43185a7bdbec5d3c574b2b8729d8b079b1fad5
Mamun-Ashraf <ashrafdkia@gmail.com>
2024-01-20 15:46:19 +0600

    added actions and payload

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useSelector } from "react-redux"
-import {RootState} from './redux/store';
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from './redux/store';
+import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice";
 
 function App() {
 
-  const {count} = useSelector((state : RootState) => state.counter)
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div>
       <div className='flex gap-4 m-5'>
-        <button className="btn btn-outline btn-success">Increment</button>
+        <button className="btn btn-outline btn-success" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button className="btn btn-outline btn-success" onClick={() => dispatch(incrementByAmount(5))}>
+          Increment by 5
+        </button>
         <div>{count}</div>
-        <button className="btn btn-outline btn-error">Decrement</button>
+        <button className="btn btn-outline btn-error" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
       </div>
     </div>
   )
