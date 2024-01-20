ec234d296b84a7dd3684c47f89ad40c0a253419a
Mamun-Ashraf <ashrafdkia@gmail.com>
2024-01-20 15:25:12 +0600

    setup redux store, slice and connect store with component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux"
+import {RootState} from './redux/store';
+
 function App() {
 
+  const {count} = useSelector((state : RootState) => state.counter)
+
   return (
-    <>
-      <h2 className="text-4xl">My first challenge</h2>
-    </>
+    <div>
+      <div className='flex gap-4 m-5'>
+        <button className="btn btn-outline btn-success">Increment</button>
+        <div>{count}</div>
+        <button className="btn btn-outline btn-error">Decrement</button>
+      </div>
+    </div>
   )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/Home";
+// import Home from "../pages/Home";
 import Login from "../pages/Login";
+import NotFound from "../pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
     {
@@ -9,13 +10,13 @@ const router = createBrowserRouter([
         element: <App />
     },
     {
-        path: '/home',
-        element: <Home />
-    },
-    {
         path: '/login',
         element: <Login></Login>
-    }
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    },
 ])
 
 export default router;
